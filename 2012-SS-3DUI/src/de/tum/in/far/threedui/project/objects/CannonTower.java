f80d2318589c8a43227aa416194b33b769723f4e
agsgde <agsgde@gmail.com>
2012-07-09 21:43:29 +0000

    Towers can now shoot (somehow...) Todo: proper starting position of projectiles textures for projectiles collision detection between enemies and projectiles

--- 2012-SS-3DUI/src/de/tum/in/far/threedui/project/objects/CannonTower.java
+++ 2012-SS-3DUI/src/de/tum/in/far/threedui/project/objects/CannonTower.java
@@ -1,11 +1,16 @@
 package de.tum.in.far.threedui.project.objects;
 
+import java.awt.Font;
 import java.awt.Point;
 import java.util.Enumeration;
 
 import javax.media.j3d.Behavior;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Font3D;
+import javax.media.j3d.Geometry;
+import javax.media.j3d.OrientedShape3D;
+import javax.media.j3d.QuadArray;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.media.j3d.WakeupCondition;
@@ -16,6 +21,7 @@ import javax.vecmath.Point3f;
 import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
 
+import de.tum.in.far.threedui.general.BlueAppearance;
 import de.tum.in.far.threedui.general.TransformableObject;
 import de.tum.in.far.threedui.project.core.ModelLoader;
 import de.tum.in.far.threedui.project.core.ModelLoader.ModelFormat;
@@ -29,6 +35,10 @@ public class CannonTower extends TransformableObject{
 	private CannonTowerController controller;
 	private float barrelYOffset = 0.02f;
 	private Type towerType;
+	private int fireRate = 5;
+	private float turretAngle = 0;
+	private float barrelAngle = 0;
+	private float projectileLifetime;
 	
 	private TransformGroup turretRotGroup;
 	private TransformGroup barrelRotGroup;
@@ -66,6 +76,13 @@ public class CannonTower extends TransformableObject{
 	{
 		this.towerType = type;
 		this.setCapability(BranchGroup.ALLOW_DETACH);
+		this.setCapability(BranchGroup.ALLOW_CHILDREN_EXTEND);
+
+		this.setCapability(BranchGroup.ALLOW_CHILDREN_READ);
+
+		this.setCapability(BranchGroup.ALLOW_CHILDREN_WRITE);
+		
+
 		
 		TransformableObject cannonTop = null;
 		TransformableObject cannonBase = null;
@@ -79,6 +96,7 @@ public class CannonTower extends TransformableObject{
 			cannonTop = this.loader.getModelObject("Gun-Top");
 			cannonBase = this.loader.getModelObject("Gun-Base");
 			cannonBarrel = this.loader.getModelObject("Gun-Barrel");
+			projectileLifetime = 50;
 			
 			barrelYOffset = 0.02f;
 		}
@@ -88,13 +106,16 @@ public class CannonTower extends TransformableObject{
 			cannonBase = this.loader.getModelObject("DoubleGun-Base");
 			cannonBarrel = this.loader.getModelObject("DoubleGun-Barrel");
 			barrelYOffset = 0.016f;
+			projectileLifetime = 50;
 		}
 		else if(type == Type.PRECISION)
 		{
 			cannonTop = this.loader.getModelObject("Precision-Top");
 			cannonBase = this.loader.getModelObject("Precision-Base");
 			cannonBarrel = this.loader.getModelObject("Precision-Barrel");
-			barrelYOffset = 0.02f;
+			barrelYOffset = 0.024f;
+			projectileLifetime = 200;
+			
 		}
 		else if(type == Type.GATLING)
 		{
@@ -102,6 +123,9 @@ public class CannonTower extends TransformableObject{
 			cannonBase = this.loader.getModelObject("Gatling-Base");
 			cannonBarrel = this.loader.getModelObject("Gatling-Barrel");
 			barrelYOffset = 0.013f;
+			
+			projectileLifetime = 100;
+			
 		}
 		else if(type == Type.HOWITZER)
 		{
@@ -109,6 +133,8 @@ public class CannonTower extends TransformableObject{
 			cannonBase = this.loader.getModelObject("Howitzer-Base");
 			cannonBarrel = this.loader.getModelObject("Howitzer-Barrel");
 			barrelYOffset = 0.013f;
+			
+			projectileLifetime = 500;
 		}
 		
 		
@@ -153,6 +179,9 @@ public class CannonTower extends TransformableObject{
 		
 		super.transGroup.addChild(controller);
 		super.transGroup.addChild(bGroup);
+		
+	
+		
 		
 	}
 	
@@ -215,13 +244,15 @@ public class CannonTower extends TransformableObject{
 	
 	public void setTurretAngle(float angle)
 	{
-
+		this.turretAngle = angle;
+		
 		turretRotation.rotY(angle);
 		turretRotGroup.setTransform(turretRotation);
 	}
 	
 	public void setBarrelElevation(float angle)
 	{
+		this.barrelAngle = angle;
 		Transform3D barrelTranslation = new Transform3D();
 		barrelTranslation.setTranslation(new Vector3f(0.0f,barrelYOffset,0.0f));
 		barrelElevation.rotX(angle);
@@ -232,6 +263,37 @@ public class CannonTower extends TransformableObject{
 	public Type getType()
 	{
 		return this.towerType;
+	}
+	
+	public int getFireRate()
+	{
+		return this.fireRate;
+	}
+	
+	public Vector3f getAimVector()
+	{
+		
+		Vector3f v = new Vector3f(0.0f,-1.0f,0.0f);
+
+		barrelElevation.rotX(barrelAngle);
+		barrelElevation.transform(v);
+		turretRotation.rotZ(turretAngle);
+		turretRotation.transform(v);
+		
+		
+		System.out.println(v);
+		return v;
+		
+		
+	}
+	public float getProjectileLifetime()
+	{
+		return this.projectileLifetime;
+	}
+	
+	public Vector3f getBarrelTip()
+	{
+		return new Vector3f(0,this.barrelYOffset,0);
 	}
 
 }
